78d74e3664cf93872285e37c3d806567c0020fee
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-04-25 08:20:00 -0600

    Odd Range

---
+++ Ruby/arrays/19_OddRange.rb
@@ -0,0 +1,21 @@
+# Odd Range
+# Write a method odd_range(min, max) that takes in two numbers min and max.
+# The method should return an array containing all odd numbers from min to max (inclusive).
+
+def odd_range(min, max)
+  new_array = []
+  i = min
+
+  while i <= max
+    if i % 2 != 0
+      new_array << i
+    end
+    i += 1
+  end
+
+  return new_array
+end
+
+print odd_range(11, 18) # => [11, 13, 15, 17]
+puts
+print odd_range(3, 7)   # => [3, 5, 7]
