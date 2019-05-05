e875dd0a22daf95120ba91522c047863c3b7fc65
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-05-05 06:56:01 -0600

    To Initials

---
+++ Ruby/enumerables_and_ranges/27_ToInitials.rb
@@ -0,0 +1,16 @@
+# To Initials
+# Write a method to_initials that takes in a person's name string
+# and returns a string representing their initials.
+
+def to_initials(name)
+  initials = ""
+  name.split.each do |char|
+    initials += char[0]
+  end
+
+  return initials
+end
+
+puts to_initials("Kelvin Bridges")      # => "KB"
+puts to_initials("Michaela Yamamoto")   # => "MY"
+puts to_initials("Mary La Grange")      # => "MLG"
