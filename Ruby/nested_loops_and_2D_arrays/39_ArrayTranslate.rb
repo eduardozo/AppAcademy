f5f6c90656defe095b927d43c622872b26953d7c
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-05-16 07:40:00 -0600

    Array Translate

---
+++ Ruby/nested_loops_and_2D_arrays/39_ArrayTranslate.rb
@@ -0,0 +1,24 @@
+# Array Translate
+# Write a method array_translate that takes in an array whose elements alternate
+# between words and numbers. The method should return a string where each word is repeated
+# the number of times that immediately follows in the array.
+
+def array_translate(array)
+  sentence = ""
+  i = 0
+  while i < array.length
+    ele = array[i]
+    num = array[i + 1]
+    num.times { sentence += ele }
+
+    i += 2
+  end
+
+  return sentence
+end
+
+print array_translate(["Cat", 2, "Dog", 3, "Mouse", 1]); # => "CatCatDogDogDogMouse"
+puts
+
+print array_translate(["red", 3, "blue", 1]); # => "redredredblue"
+puts
