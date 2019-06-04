44004526e187badfabd415103c65987b0700d9b4
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-06-04 09:10:51 -0600

    Last Index

---
+++ Ruby/advance_problems/59_LastIndex.rb
@@ -0,0 +1,12 @@
+# Last Index
+# Write a method last_index that takes in a string and a character.
+# The method should return the last index where the character can be found in the string.
+
+def last_index(str, char)
+  return str.chars.each_with_index.select { |key, value| key == char }.map { |found| found.last }.last
+end
+
+puts last_index("abca", "a")       #=> 3
+puts last_index("mississipi", "i") #=> 9
+puts last_index("octagon", "o")    #=> 5
+puts last_index("programming", "m")#=> 7
