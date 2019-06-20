45a1fe23610db0baf96cb3a3e8f49fefbc908eff
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-06-20 09:22:45 -0600

    Same Char Collapse Solution 2

--- Ruby/advance_problems/77_SameCharCollapse.rb
+++ Ruby/advance_problems/77_SameCharCollapse.rb
@@ -29,3 +29,25 @@ puts same_char_collapse("zzzxaaxy")   #=> "zy"
 
 puts same_char_collapse("uqrssrqvtt") #=> "uv"
 # because uqrssrqvtt -> uqrrqvtt -> uqqvtt -> uvtt -> uv
+#
+# Solution 2
+# def same_char_collapse(str)
+#   reducible = true
+# 
+#   while reducible
+#     chars = str.split("")
+#     reducible = false
+#
+#     chars.each.with_index do |char, i|
+#       if chars[i] == chars[i + 1]
+#         chars[i] = ""
+#         chars[i + 1] = ""
+#         reducible = true
+#       end
+#     end
+#
+#     str = chars.join("")
+#   end
+#
+#   return str
+# end
