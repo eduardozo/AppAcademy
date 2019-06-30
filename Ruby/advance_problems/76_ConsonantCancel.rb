cb3afb101bbfcefc7a3d65a26a1bfa3cb8b9cfeb
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-06-29 22:49:01 -0600

    Consonant Cancel

---
+++ Ruby/advance_problems/76_ConsonantCancel.rb
@@ -0,0 +1,21 @@
+# Consonant Cancel
+# Write a method consonant_cancel that takes in a sentence
+# and returns a new sentence where every word begins with it's first vowel.
+
+def consonant_cancel(sentence)
+  arr_sentence = []
+
+  sentence.split.each do |word|
+    word.chars.each_with_index do |char, i|
+      if ('aeiou').include?(char)
+        arr_sentence << word[i..-1]
+        break
+      end
+    end
+  end
+
+  return arr_sentence.join(" ")
+end
+
+puts consonant_cancel("down the rabbit hole") #=> "own e abbit ole"
+puts consonant_cancel("writing code is challenging") #=> "iting ode is allenging"
