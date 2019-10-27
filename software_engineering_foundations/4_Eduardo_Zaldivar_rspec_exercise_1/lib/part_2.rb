c9597f2573b106535b44d932941d3b535a46ac0e
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-10-27 09:51:27 -0600

    ceasar cipher

--- software_engineering_foundations/4_Eduardo_Zaldivar_rspec_exercise_1/lib/part_2.rb
+++ software_engineering_foundations/4_Eduardo_Zaldivar_rspec_exercise_1/lib/part_2.rb
@@ -14,3 +14,19 @@ def vowel_counts(string)
   string.downcase.chars { |char| 'aeiou'.include?(char) ? hash[char] += 1 : 0 }
   hash
 end
+
+def caesar_cipher(string, num)
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'
+  cipher = ''
+
+  (0...string.length).each do |i|
+    char = string[i]
+    if alphabet.include?(char)
+      index = alphabet.index(char) + num
+      cipher += alphabet[index % 26]
+    else
+      cipher += char
+    end
+  end
+  cipher
+end
