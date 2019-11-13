4b9fb4fe7172d98876b193ccf2883938ed7f9101
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2019-11-13 08:13:59 -0700

    reverse word method

--- software_engineering_foundations/5_Eduardo_zaldivar_rspec_exercise_2/lib/part_2.rb
+++ software_engineering_foundations/5_Eduardo_zaldivar_rspec_exercise_2/lib/part_2.rb
@@ -2,3 +2,14 @@ def palindrome?(string)
   return string == reverse(string)
 end
 
+def reverse(word)
+  i = word.length - 1
+  w_reverse = ''
+  while i >= 0
+    w_reverse += word[i]
+    i -= 1
+  end
+
+  w_reverse
+end
+
