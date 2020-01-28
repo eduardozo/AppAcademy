1a907c3721d86df9ddabd20d574e5946ee0d05d0
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-01-28 13:20:09 -0700

    no valid url

--- software_engineering_foundations/8_Eduardo_Zaldivar_blocks_project/lib/part_2.rb
+++ software_engineering_foundations/8_Eduardo_Zaldivar_blocks_project/lib/part_2.rb
@@ -2,3 +2,9 @@ def all_words_capitalized?(array)
   array.all? { |ele| ele == ele.capitalize }
 end
 
+def no_valid_url?(array)
+  url_end = ['.com', '.net', '.io', '.org']
+
+  array.none? { |url| url_end.include?(url.slice(-4..-1)) }
+end
+
