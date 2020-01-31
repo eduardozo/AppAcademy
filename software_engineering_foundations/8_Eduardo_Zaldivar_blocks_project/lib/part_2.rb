4248f3d99fc1515aa5ab0a809b4f0f5fe31bf53c
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-01-31 07:21:30 -0700

    any passing students?

--- software_engineering_foundations/8_Eduardo_Zaldivar_blocks_project/lib/part_2.rb
+++ software_engineering_foundations/8_Eduardo_Zaldivar_blocks_project/lib/part_2.rb
@@ -8,3 +8,6 @@ def no_valid_url?(array)
   array.none? { |url| url_end.include?(url.slice(-4..-1)) }
 end
 
+def any_passing_students?(students)
+  students.any? { |student| student[:grades].sum / student[:grades].size > 75 }
+end
