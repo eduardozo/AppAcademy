4fc203bcdd5428f37eb79469279c587204360c70
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-02-29 08:23:22 -0700

    greater proc value

--- software_engineering_foundations/9_Eduardo_zaldivar_procs_project/lib/part_2.rb
+++ software_engineering_foundations/9_Eduardo_zaldivar_procs_project/lib/part_2.rb
@@ -9,3 +9,10 @@ def word_changer(string, &proc)
   new_str.join(' ')
 end
 
+def greater_proc_value(num, proc_1, proc_2)
+  result_one = proc_1.call(num)
+  result_two = proc_2.call(num)
+
+  result_one > result_two ? result_one : result_two
+end
+
