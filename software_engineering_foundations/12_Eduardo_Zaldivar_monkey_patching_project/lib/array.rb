ce751aea127da61492614b697109795be4d5dbd3
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-03-25 07:19:32 -0600

    median - own custom method

--- software_engineering_foundations/12_Eduardo_Zaldivar_monkey_patching_project/lib/array.rb
+++ software_engineering_foundations/12_Eduardo_Zaldivar_monkey_patching_project/lib/array.rb
@@ -13,6 +13,17 @@ class Array
     sum / size.to_f
   end
 
+  def median
+    return nil if empty?
+
+    mid_index = length / 2
+    sorted_arr = sort
+
+    return sorted_arr[mid_index] if length.odd?
+
+    (sorted_arr[mid_index] + sorted_arr[mid_index - 1]) / 2.0 if length.even?
+  end
+
 
 
 end
