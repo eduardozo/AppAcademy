8ceee507617133ab6d80b13c08aa59deb86328fc
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-05-04 13:12:22 -0600

    gamme over? method

--- software_engineering_foundations/14_Eduardo_Zaldivar_hangman_project/lib/hangman.rb
+++ software_engineering_foundations/14_Eduardo_Zaldivar_hangman_project/lib/hangman.rb
@@ -76,6 +76,13 @@ class Hangman
     false
   end
 
-
+  def game_over?
+    if self.win? || self.lose?
+      puts @secret_word
+      return true
+    else
+      return false
+    end
+  end
 
 end
