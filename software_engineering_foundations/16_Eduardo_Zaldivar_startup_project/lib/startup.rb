56886c208797e3d3ccce849788e865480efab1bc
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-06-08 08:55:33 -0600

    class startup

---
+++ software_engineering_foundations/16_Eduardo_Zaldivar_startup_project/lib/startup.rb
@@ -0,0 +1,16 @@
+require 'employee'
+
+class Startup
+
+  attr_reader :name, :funding, :salaries, :employees
+
+  def initialize(name, funding, salaries)
+    @name = name
+    @funding = funding
+    @salaries = salaries
+    @employees = []
+  end
+
+
+
+end
