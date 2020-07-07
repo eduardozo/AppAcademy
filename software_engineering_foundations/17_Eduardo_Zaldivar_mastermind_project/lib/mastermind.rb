0e07c94c98bb56a75ab6d47895ada59fb2a9ae17
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-07-07 12:10:33 -0600

    print matches mastermind

--- software_engineering_foundations/17_Eduardo_Zaldivar_mastermind_project/lib/mastermind.rb
+++ software_engineering_foundations/17_Eduardo_Zaldivar_mastermind_project/lib/mastermind.rb
@@ -6,6 +6,14 @@ class Mastermind
     @secret_code = Code.random(length)
   end
 
+  def print_matches(code)
+    matches = @secret_code.num_exact_matches(code)
+    near_matches = @secret_code.num_near_matches(code)
+
+    puts "Number of exact matches: #{matches}"
+    puts "Number of near matches: #{near_matches}"
+  end
+
 
 
 end
