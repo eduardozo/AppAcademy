01d31565722d0b15b0a7f97fbdf798cd7d79e722
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-07-14 09:11:46 -0600

    class code

---
+++ software_engineering_foundations/17_Eduardo_Zaldivar_mastermind_project/lib/code.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class Code
+  attr_reader :pegs
+
+  POSSIBLE_PEGS = {
+      'R' => :red,
+      'G' => :green,
+      'B' => :blue,
+      'Y' => :yellow
+  }.freeze
+
+
+
+end
