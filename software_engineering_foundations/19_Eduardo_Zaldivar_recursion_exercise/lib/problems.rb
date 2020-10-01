6a502beaecf41fd205ed6a06bb57233191283267
Eduardo Zaldivar <ezaldivar@jaumebalmes.net>
2020-10-01 17:23:16 -0600

    recursive flatten data

--- software_engineering_foundations/19_Eduardo_Zaldivar_recursion_exercise/lib/problems.rb
+++ software_engineering_foundations/19_Eduardo_Zaldivar_recursion_exercise/lib/problems.rb
@@ -81,3 +81,51 @@ def reverse_string(str)
 end
 
 
+# A 1-dimensional array is also known as a flattened array.
+# Write a method, flatten(data), that accepts a single argument. The
+# method should take in an array of any dimension and return the flattened
+# version of that array. Solve this recursively.
+#   
+# Hint:
+#  - if the argument is not an array, then we have reached the base case
+#  - look up the documentation for how to check if data is an array or not
+#
+# Examples:
+#
+# array_1 = [1, 2, [[3, 4], [5, [6]]], [7, 8]]
+# flatten(array_1)      # => [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+#
+# array_2 = ['this', ['problem', 'is'], [['pretty', 'tough'], [[':)']]]]
+# flatten(array_2)      # => [ 'this', 'problem', 'is', 'pretty', 'tough', ':)' ]
+#
+# flatten('base case')  # => [ 'base case' ]
+#
+# Another Hint:
+#
+# From the last example, you may be confused. We said that the method takes
+# in an array as an arg, but we passed it a string?
+# If data is not an array, then we can consider it as a 0-dimensional array.
+#     0-dimensional array: 'some data'
+#     1-dimensional array: ['some data']
+#     2-dimensional array: [['some data']]
+#     3-dimensional array: [[['some data']]]
+def flatten(data)
+  return [data] if data.instance_of?(Array) == false
+
+  flattened = []
+
+  data.each do |ele|
+    flattened.concat(flatten(ele))
+  end
+
+  flattened
+end
+
+# length = data.length - 1
+# if data.first.is_a?(Array)
+#   flattened.concat(flatten(data.first))
+# else
+#   index = data.index(data.first)
+#   flattened << data.first
+#   flatten(data[index + 1])
+# end
